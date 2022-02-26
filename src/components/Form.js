edaf440e1307e003d30c225770702b766b6ee086
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-02-25 21:38:21 -0700

    complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
+function Form({
+  firstName,
+  lastName,
+  newsletter,
+  handleFirstNameChange,
+  handleLastNameChange,
+  handleNewsletterChange,
+}) {
+  const [number, setNumber] = useState(0);
+  const [error, setError]= useState(null);
+
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setError(null)
+    }else{
+      setError(`${newNumber} is not a valid number!`)
+    }
+  }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} placeholder="First Name"/>
+      <input type="text" onChange={handleLastNameChange} value={lastName} placeholder="Last Name"/>
+      <input type="checkbox" id="newsletter" onChange={handleNewsletterChange} checked={newsletter} />
+      <input type="number" value={number} onChange={handleNumberChange} />
+      {error ? <span style={{ color: "red" }}>{error}</span> : null}
       <button type="submit">Submit</button>
     </form>
   );
